aa7a97036726b6f49d38e80913fdd48e6aaba8e2
Matthew <matthew.97.clark@gmail.com>
2019-11-08 23:36:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,8 @@ end
 def play(songs)
   puts "/Please enter a song name or number:/"
   input = gets.strip
-  if input.to_i == 
+  if songs[input.to_i] != nil
+    puts songs[input.to_i - 1]
   end
   # binding.pry
 end 
