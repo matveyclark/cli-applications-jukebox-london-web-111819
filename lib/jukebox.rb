3bae2a40fd7183ce4106e586b84d1ffde120e4a8
Matthew <matthew.97.clark@gmail.com>
2019-11-08 23:39:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.downcase.chomp 
+  user_response = gets.split
 
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
